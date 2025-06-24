1e85d8e017f1dbdc8325d0b624383fa2db13486d
NikhilBaswal <nikhilbaswal7127@gmail.com>
2025-06-24 15:15:13 +0530

    Add files via upload

---
+++ programming basics/functions.js
@@ -0,0 +1,21 @@
+function number0ton(n){
+    for(i=0;i<=n;i++)
+        console.log(i)
+    console.log()
+}
+
+number0ton(5);
+number0ton(7);
+  
+function sum(a,b,c){
+    console.log(Math.abs (a*a*a)+ Math.abs (b*b*b) + Math.abs(c*c*c))   // we use math.abs for +ve value
+    
+}
+
+sum(-2,1,3)
+
+function me(){
+    return "i am Nikhil"
+   
+}
+console.log(me())
